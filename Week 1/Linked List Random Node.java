03d9b322c7dd2ced85dca5e306afebd6d3904ea7
AKM_Curious <amithkmbvr@gmail.com>
2020-12-03 09:41:42 -0600

    add solution to december 2nd challenge

---
+++ Week 1/Linked List Random Node.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    ListNode head;
+    
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        if(head == null)
+            return 0;
+        long len = 1;
+        int ans = head.val;
+        ListNode curr = head;
+        while(curr != null)
+        {
+            if((Math.random()*len) < 1)
+                ans = curr.val;
+            curr = curr.next;
+            len++;
+        }
+        return ans;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
